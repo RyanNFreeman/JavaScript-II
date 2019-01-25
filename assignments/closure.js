75643ec1d83c1ff7761f4eb19606aa0d0d9b3d31
RyanNFreeman <ryan.norman.freeman@gmail.com>
2019-01-25 00:12:28 -0700

    added solution

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,14 +1,29 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function sayHello() {
+  let name = "Jim";
+  function slang() {
+    console.log('Yo ' + name);
+  }
+  slang();
+}
+console.log(sayHello());
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return() => (++count);
 };
+
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
+const newCounter = counter();
+
+console.log(newCounter());
+console.log(newCounter())
 
 /* STRETCH PROBLEM, Do not attempt until you have completed all previous tasks for today's project files */
 
